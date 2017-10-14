5bd8cff4577aa6ae4608b4a5a6ae46a5771eda7c
Tommys Christou <tommys@mds.com.cy>
2017-10-14 14:13:32 +0300

    remove author var

--- inc/theme-plugins.php
+++ inc/theme-plugins.php
@@ -6,9 +6,7 @@
 add_action('admin_notices', 'theme_plugin_dependencies');
 function theme_plugin_dependencies($checkonly = null) {
 	$theme = wp_get_theme();
-	$author = ($theme && $theme->exists() && $theme['author']) ? $theme['author'] : 'your Wordpress-theme developer';
 	$format = '<div class="notice notice-error"><p>Theme required plugin %s: %s</p></div>';
-
 	$plugins = array(
 		'advanced-custom-fields/acf.php' => array(
 			'name' => '<a href="https://www.advancedcustomfields.com/" target="_blank">Advanced Custom Fields</a>',
@@ -32,7 +30,7 @@ function theme_plugin_dependencies($checkonly = null) {
 	foreach ($plugins as $plugin => $nfo) {
 		if (is_wp_error(validate_plugin($plugin))) {
 			if (!$nfo['slug']) {
-				$out .= sprintf($format, $nfo['name'], "Please contact $author for installation instructions.");
+				$out .= sprintf($format, $nfo['name'], "Please contact your developer for installation instructions.");
 			} else {
 				$link = wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=' . $nfo['slug']), 'install-plugin_' . $nfo['slug']);
 				$out .= sprintf($format, $nfo['name'], "Please <a href='$link'>install</a> this Plugin.");
