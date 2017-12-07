b206ea30abf14d665e7fbae061a45c5d68a5440d
Tommys Christou <tommys@mds.com.cy>
2017-12-07 13:48:51 +0200

    Fix Single

--- single.php
+++ single.php
@@ -3,14 +3,18 @@ use Timber\Helper;
 use Timber\Post;
 use Timber\Timber;
 
-$context         = Timber::get_context();
-$post            = new Post();
+$context = Timber::get_context();
+$post = Timber::query_post();
 $context['post'] = $post;
-if (comments_open()) {
-    $context['comment_form'] = Helper::get_comment_form();
-}
-if (post_password_required($post->ID)) {
-    Timber::render(['single-password.twig'], $context);
+$templates = array( 
+	'single-' . $post->post_type . '-' . $post->slug . '.twig', 
+	'single-' . $post->ID . '.twig', 
+	'single-' . $post->post_type . '.twig',
+	'single.twig'
+);
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
 } else {
-    Timber::render(['single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'], $context);
+	Timber::render( $templates, $context );
 }
